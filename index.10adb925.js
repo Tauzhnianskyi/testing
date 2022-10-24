67c1b97bbbde06f9bd272704dbc5f9bb2c484b22
Tauzhnianskyi <104843369+Tauzhnianskyi@users.noreply.github.com>
2022-10-24 19:03:43 +0000

    Deploying to gh-pages from @ Tauzhnianskyi/testing@23c69d63a6bceea56bef245bcd8d08d7cd0668d3 🚀

---
+++ index.10adb925.js
@@ -0,0 +1,2 @@
+fetch("https://the-one-api.dev/v2/character?limit=10&page=1",{headers:{Authorization:"Bearer XJlq9OFMcHAy8pAQK7xj"}}).then((e=>{if(!e.ok)throw new Error("fail");return e.json()})).then(console.log);
+//# sourceMappingURL=index.10adb925.js.map
---
+++ index.7a269404.js
@@ -0,0 +1,2 @@
+!function(){var t={headers:{Authorization:"Bearer XJlq9OFMcHAy8pAQK7xj"}};fetch("".concat("https://the-one-api.dev/v2/").concat("character","?limit=10&page=1"),t).then((function(t){if(!t.ok)throw new Error("fail");return t.json()})).then(console.log)}();
+//# sourceMappingURL=index.7a269404.js.map
