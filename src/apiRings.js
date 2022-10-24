23c69d63a6bceea56bef245bcd8d08d7cd0668d3
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-10-24 22:03:14 +0300

    xz

---
+++ src/apiRings.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://the-one-api.dev/v2/';
+const character = 'character';
+const bearer = 'Bearer XJlq9OFMcHAy8pAQK7xj';
+
+const options = {
+  headers: {
+    Authorization: bearer,
+  },
+};
+
+export function apiRings() {
+  return fetch(`${BASE_URL}${character}?limit=10&page=1`, options).then(
+    response => {
+      if (!response.ok) {
+        throw new Error('fail');
+      }
+      return response.json();
+    }
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// const base_url = 'http://api.weatherapi.com/v1';
+// const KEY = '4202b3fa59ea4adf832162138221110';
+// const container = document.querySelector('.list');
+// const cityName = document.querySelector('.city');
+// const form = document.querySelector('form');
+
+import { apiRings } from './apiRings';
+
+apiRings().then(console.log);
